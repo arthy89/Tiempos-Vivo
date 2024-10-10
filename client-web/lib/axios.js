056d021750151cc7138ab2686fa3ffae625839b1
arthy89 <arhyel.860@gmail.com>
2024-10-10 15:54:27 -0500

    config laptop

--- client-web/lib/axios.js
+++ client-web/lib/axios.js
@@ -2,8 +2,9 @@ import Axios from 'axios';
 import { client } from 'laravel-precognition-react';
 const api = Axios.create({
   // baseURL: process.env.APP_API_URL,
-//   baseURL: 'http://sigetec-admin.test/',
-  baseURL: 'http://tev.test/',
+  baseURL: process.env.NEXT_PUBLIC_SERVER_URI,
+  //   baseURL: 'http://sigetec-admin.test/',
+  // baseURL: 'http://tev.test/',
 
   // headers: {
   //   'X-Requested-With': 'XMLHttpRequest',
