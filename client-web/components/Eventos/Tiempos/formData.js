eaceb004337284b02fd5e51d2828fd073006edb1
arthy89 <arhyel.860@gmail.com>
2024-10-19 03:36:43 -0500

    comillas por prettier

--- client-web/components/Eventos/Tiempos/formData.js
+++ client-web/components/Eventos/Tiempos/formData.js
@@ -1,12 +1,11 @@
 export const formData = {
   id: 0,
-
-  especial_id: '',
-  shakedown_id: '',
-  tripulacion_id: '',
-  hora_salida: '',
-  hora_llegada: '00:00:00',
-  hora_marcado: '',
-  penalizacion: '',
-  registrador: '',
+  especial_id: "",
+  shakedown_id: "",
+  tripulacion_id: "",
+  hora_salida: "",
+  hora_llegada: "00:00:00",
+  hora_marcado: "",
+  penalizacion: "",
+  registrador: "",
 };
