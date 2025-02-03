6a6b0dfa4d8df82326825dfa715db62310590a2f
arthy89 <arhyel.860@gmail.com>
2025-02-03 11:54:07 -0500

    buscardor de eventos

--- client-web/components/Eventos/EventoList.jsx
+++ client-web/components/Eventos/EventoList.jsx
@@ -5,7 +5,7 @@ function EventoList({ eventos }) {
   // console.log("evento:", eventos);
 
   return (
-    <div className="grid min-[320px]:max-md:grid-cols-1 md:grid-flow-col md:auto-cols-max gap-4">
+    <div className="grid min-[320px]:max-md:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
       {eventos.data.map((evento) => (
         <EventoCard evento={evento} key={evento.id} />
       ))}
